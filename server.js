c042ba12434356d3f340e19f89f86a4d1fc71977
Link Strifer <linkstrifer@outlook.com>
2020-12-10 18:35:31 -0500

    add missing dependency

--- server.js
+++ server.js
@@ -1,91 +1,88 @@
-const express = require ("express");
-const cors = require ("cors");
-const bodyParser = require ("body-parser");
-const mongoose = require ("mongoose");
-const { isObject } = require("util");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/chat', { 
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/chat", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 mongoose.connection.on("error", (error) => {
-    console.log (`MONGOOSE ERROR: ${error}`);
-    });
+  console.log(`MONGOOSE ERROR: ${error}`);
+});
 
 const MessageSchema = mongoose.Schema({
-    channel: String,
-    text: String,
-    user: String, 
-    timestamp: { 
-        type: Date,
-        default: new Date() 
-    },
-}); 
+  channel: String,
+  text: String,
+  user: String,
+  timestamp: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-const MessageModel= mongoose.model("Message", MessageSchema);
+const MessageModel = mongoose.model("Message", MessageSchema);
 
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-       cors: {
-        origin: "*",
-        methods: ["GET" , "POST"],
-       }
-    }
-)
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 const port = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 //## Endpoints
 
-//### GET channel messages (primer nodo)  
+//### GET channel messages (primer nodo)
 
 //- GET /api/channel/:channelId
- // - { messages }
+// - { messages }
 
-app.get("/channel/:id", (req,res) => {
-    const { id } = req.params; 
+app.get("/channel/:id", (req, res) => {
+  const { id } = req.params;
 
-    MessageModel.find({channel : id})
+  MessageModel.find({ channel: id })
     .then((messages) => {
-    res.status(200).json({ messages });
+      res.status(200).json({ messages });
     })
     .catch((error) => {
-    res.status(500).json({ error });
-});
+      res.status(500).json({ error });
+    });
 });
 
 //- POST /api/user/create
 
-app.post("/channel/:id", (req,res) => {
-    const { id } = req.params; 
-    const { text , user } = req.body;
-    console.log(text , user)
- 
-    if (text && user) {
-        MessageModel.create({user , text , channel: id})
-        .then((result) => {
-        io.emit("newMessage", result );    
+app.post("/channel/:id", (req, res) => {
+  const { id } = req.params;
+  const { text, user } = req.body;
+  console.log(text, user);
+
+  if (text && user) {
+    MessageModel.create({ user, text, channel: id })
+      .then((result) => {
+        io.emit("newMessage", result);
         res.status(200).json(result);
-    })
-        .catch((error) => {
-        res.status(500).json({error});
-    });
-    } else {
-        res.status(400).json({error : "missing some parameters"});
-    }
-  
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  } else {
+    res.status(400).json({ error: "missing some parameters" });
+  }
 });
 
-//Sockets 
-io.on('connection', () => {
-    console.log("new conection sockets")
+//Sockets
+io.on("connection", () => {
+  console.log("new connection sockets");
 });
 
-server.listen(port, () => { 
-console.log(`Listening on port ${port}`)
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
